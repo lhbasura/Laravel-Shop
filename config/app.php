890689080213ec987ffd53d01a4de1f3a8d1269a
lhbasura <lhbasura@gmail.com>
2019-04-08 22:14:16 +0800

    商品模型

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://shop.test'),
 
     'asset_url' => env('ASSET_URL', null),
 
--- config/filesystems.php
+++ config/filesystems.php
@@ -63,6 +63,14 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
         ],
+        'admin' => [
+            'driver' => 'local',
+            'root' => public_path('uploads'),
+            'visibility' => 'public',
+            'url' => env('APP_URL').'/uploads',
+        ],
+
+
 
     ],
 
