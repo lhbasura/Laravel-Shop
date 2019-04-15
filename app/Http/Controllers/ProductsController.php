b233d33b4bdf2be4f7f0e2b350c98a5517e22e90
lhbasura <lhbasura@gmail.com>
2019-04-15 21:59:51 +0800

    异常处理

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use App\Models\Product;
 
 use Illuminate\Http\Request;
@@ -55,7 +56,7 @@ class ProductsController extends Controller
     public  function  show(Product $product, Request $request)
     {
         if (!$product->on_sale) {
-            throw new \Exception('商品未上架');
+            throw new InvalidRequestException('商品未上架');
         }
 
         return view('products.show', ['product' => $product]);
