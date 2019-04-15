dead3be4f24f37e592c66a8346b08243d785515c
lhbasura <lhbasura@gmail.com>
2019-04-15 22:28:40 +0800

    用户界面 - 商品收藏

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ Route::group(['middleware' => ['auth', 'verified' , 'address']], function() {
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 
+    Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+    Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+
+
 });
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
