a9053a3aef574a65073a3b60771066811c655eeb
LiuHongbo <hongbo.liu@geneegroup.com>
2019-04-25 10:31:50 +0800

    用户界面 - 订单列表页

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,8 @@ Route::group(['middleware' => ['auth', 'verified' ]], function() {
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 
     Route::post('orders', 'OrdersController@store')->name('orders.store');
+    Route::get('orders', 'OrdersController@index')->name('orders.index');
+
 });
 
 
