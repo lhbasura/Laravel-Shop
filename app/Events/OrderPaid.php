de1be51d31290e5839b1e8e7625147ff46ec2cb1
lhbasura <lhbasura@gmail.com>
2019-05-04 00:40:03 +0800

    完善支付后逻辑"

---
+++ app/Events/OrderPaid.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
+class OrderPaid
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+    protected $order;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        //
+        $this->order=$order;
+    }
+
+    public  function getOrder()
+    {
+        return $this->order;
+    }
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\OrderPaid;
 use App\Exceptions\InvalidRequestException;
 use App\Models\Order;
 use Illuminate\Http\Request;
@@ -34,7 +35,6 @@ class PaymentController extends Controller
         } catch (\Exception $e) {
             return view('pages.error', ['msg' => '数据不正确']);
         }
-
         return view('pages.success', ['msg' => '付款成功']);
     }
     public function alipayNotify()
@@ -63,8 +63,13 @@ class PaymentController extends Controller
             'payment_method' => 'alipay', // 支付方式
             'payment_no'     => $data->trade_no, // 支付宝订单号
         ]);
-
+        $this->afterPaid($order);
         return app('alipay')->success();
+    }
+
+    protected function afterPaid(Order $order)
+    {
+        event(new OrderPaid($order));
     }
 
 
