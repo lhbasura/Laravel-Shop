32450863098721ac352acf6ba506dca5b990c829
lhbasura <lhbasura@gmail.com>
2019-05-04 22:14:05 +0800

    管理后台 - 订单详情

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -19,5 +19,5 @@ Route::group([
     $router->put('products/{id}', 'ProductsController@update');
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
-
+    $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
 });
